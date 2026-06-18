100c4ad5bb2e69821fac7b04c2175b37ae1e96ad
Master <buddiemaster@gmail.com>
2026-06-18 15:55:26 -0600

    portfolio skeleton v1.0

--- js/script.js
+++ js/script.js
@@ -54,3 +54,31 @@ if(hamburger){
     });
 
 }
+// ===============================
+// Scroll Reveal Animation
+// ===============================
+
+const hiddenElements = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+});
+
+
+hiddenElements.forEach(el => {
+
+    el.classList.add("hidden");
+
+    observer.observe(el);
+
+});
